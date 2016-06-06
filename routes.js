76259f2d2a8cb2155928653843ad21b5c9bc45ce
IORTREGA <iortega@nearbpo.com>
2016-06-06 16:49:18 -0500

    Servicio opened en routes

--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ module.exports = function(app){
     global.app = app;
     
     //Sección de rutas
-    app.use('/api/bounces', require('./server/api/bounces'))
+    app.use('/api/bounces', require('./server/api/bounces'));
+    app.use('/api/opened', require('./server/api/opened'));
 }
