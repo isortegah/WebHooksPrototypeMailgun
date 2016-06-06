c70dc02cdc46fcdef9dd842514a5a210f6da0a42
IORTREGA <iortega@nearbpo.com>
2016-06-06 17:23:13 -0500

    Servicio dropped

--- routes.js
+++ routes.js
@@ -8,4 +8,5 @@ module.exports = function(app){
     app.use('/api/opened', require('./server/api/opened'));
     app.use('/api/clicked', require('./server/api/clicked'));
     app.use('/api/delivery', require('./server/api/delivery'));
+    app.use('/api/dropped', require('./server/api/dropped'));
 }
