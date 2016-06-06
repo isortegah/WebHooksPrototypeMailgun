8d552af0aa2089e703dd370016928eb788368d84
IORTREGA <iortega@nearbpo.com>
2016-06-06 17:13:34 -0500

    Ajuste a index de delivery

---
+++ server/api/delivery/index.js
@@ -0,0 +1,9 @@
+'use_strict'
+var express = require('express');
+var controller = require('./delivery.controller');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.post('/', urlencodedParser, controller.postEvento);
+module.exports = router;
